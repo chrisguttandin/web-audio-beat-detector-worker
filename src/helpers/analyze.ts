93b098f78a7a32fb3ebb77a7c3544685089a0cd4
chrisguttandin <chrisguttandin@media-codings.com>
2017-10-23 15:25:03 +0200

    throw meaningful error

--- src/helpers/analyze.ts
+++ src/helpers/analyze.ts
@@ -3,5 +3,9 @@ import { computeTempoBuckets } from './compute-tempo-buckets';
 export const analyze = (channelData: Float32Array, sampleRate: number) => {
     const tempoBuckets = computeTempoBuckets(channelData, sampleRate);
 
+    if (tempoBuckets.length === 0) {
+        throw new Error('The given channelData does not contain any detectable beats.');
+    }
+
     return tempoBuckets[0].tempo;
 };
--- src/helpers/guess.ts
+++ src/helpers/guess.ts
@@ -3,6 +3,10 @@ import { computeTempoBuckets } from './compute-tempo-buckets';
 export const guess = (channelData: Float32Array, sampleRate: number) => {
     const tempoBuckets = computeTempoBuckets(channelData, sampleRate);
 
+    if (tempoBuckets.length === 0) {
+        throw new Error('The given channelData does not contain any detectable beats.');
+    }
+
     const { peaks, tempo } = tempoBuckets[0];
     const bpm = Math.round(tempo);
     const secondsPerBeat = 60 / bpm;
