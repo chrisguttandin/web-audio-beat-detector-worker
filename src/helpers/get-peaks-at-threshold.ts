d7902a4da47efc34f36b0d099e30cdf2e93dd1e3
chrisguttandin <chrisguttandin@media-codings.com>
2018-01-29 18:00:25 +0100

    refine peak picking algorithm

--- src/helpers/get-peaks-at-threshold.ts
+++ src/helpers/get-peaks-at-threshold.ts
@@ -1,15 +1,24 @@
 export const getPeaksAtThreshold = (channelData: Float32Array, threshold: number, sampleRate: number) => {
+    const length = channelData.length;
     const peaks = [];
 
-    const length = channelData.length;
+    let lastValueWasAboveThrehold = false;
 
     for (let i = 0; i < length; i += 1) {
         if (channelData[i] > threshold) {
-            peaks.push(i);
+            lastValueWasAboveThrehold = true;
+        } else if (lastValueWasAboveThrehold) {
+            lastValueWasAboveThrehold = false;
+            peaks.push(i - 1);
 
             // Skip 0.25 seconds forward to get past this peak.
             i += (sampleRate / 4) - 1;
         }
+    }
+
+    // Add the last value in the unlikely case it was peak.
+    if (lastValueWasAboveThrehold) {
+        peaks.push(length - 1);
     }
 
     return peaks;
---
+++ test/unit/helpers/get-peaks-at-threshold.js
@@ -0,0 +1,37 @@
+import { getPeaksAtThreshold } from '../../../src/helpers/get-peaks-at-threshold';
+
+describe('getPeaksAtThreshold()', () => {
+
+    it('should pick the peak above the threshold', () => {
+        const channelData = new Float32Array([ 0, 0.3, 0.4, 0.3 ]);
+        const threshold = 0.35;
+        const sampleRate = 8;
+
+        expect(getPeaksAtThreshold(channelData, threshold, sampleRate)).to.deep.equal([ 2 ]);
+    });
+
+    it('should only pick peaks with a distance of at least 0.25 seconds', () => {
+        const channelData = new Float32Array([ 0.6, 0.3, 0.4, 0.2, 0.9, 0.1 ]);
+        const threshold = 0.35;
+        const sampleRate = 8;
+
+        expect(getPeaksAtThreshold(channelData, threshold, sampleRate)).to.deep.equal([ 0, 4 ]);
+    });
+
+    it('should wait for the hightest value before picking a peak', () => {
+        const channelData = new Float32Array([ 0.4, 0.5, 0.6, 0.7, 0.2 ]);
+        const threshold = 0.35;
+        const sampleRate = 8;
+
+        expect(getPeaksAtThreshold(channelData, threshold, sampleRate)).to.deep.equal([ 3 ]);
+    });
+
+    it('should pick a peak if it is the last value', () => {
+        const channelData = new Float32Array([ 0.2, 0.3, 0.4 ]);
+        const threshold = 0.35;
+        const sampleRate = 8;
+
+        expect(getPeaksAtThreshold(channelData, threshold, sampleRate)).to.deep.equal([ 2 ]);
+    });
+
+});
