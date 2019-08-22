c8e915555cb225c416f5fbb1f414147920b10d1f
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-08-22 15:57:49 +0200

    build(config): update linting tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,5 +1,6 @@
 module.exports = (grunt) => {
     const continuous = (grunt.option('continuous') === true);
+    const fix = (grunt.option('fix') === true);
 
     return {
         'build-es2018': {
@@ -8,8 +9,14 @@ module.exports = (grunt) => {
         'build-es5': {
             cmd: 'rollup -c config/rollup/bundle.js'
         },
-        'lint': {
+        'lint-config': {
+            cmd: `eslint --config config/eslint/config.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives *.js config/**/*.js`
+        },
+        'lint-src': {
             cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
+        },
+        'lint-test': {
+            cmd: `eslint --config config/eslint/test.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives test/**/*.js`
         },
         'test-integration': {
             cmd: `karma start config/karma/config-integration.js ${ continuous ? '--concurrency Infinity' : '--single-run' }`
