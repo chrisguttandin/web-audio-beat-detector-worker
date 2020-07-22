0aa3f11b5c5cc4fe07d768b8e978803335984066
skratchdot <git@skratchdot.com>
2020-07-22 08:37:14 -0400

    feat(tempo-settings): allow minTempo and maxTempo to be configured

--- src/helpers/analyze.ts
+++ src/helpers/analyze.ts
@@ -1,7 +1,7 @@
 import { computeTempoBuckets } from './compute-tempo-buckets';
 
-export const analyze = (channelData: Float32Array, sampleRate: number) => {
-    const tempoBuckets = computeTempoBuckets(channelData, sampleRate);
+export const analyze = (channelData: Float32Array, sampleRate: number, minTempo = 90, maxTempo = 180) => {
+    const tempoBuckets = computeTempoBuckets(channelData, sampleRate, minTempo, maxTempo);
 
     if (tempoBuckets.length === 0) {
         throw new Error('The given channelData does not contain any detectable beats.');
